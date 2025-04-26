32926f198d5b571b369cff346800d8e96263191a
Doni <laksling@gmail.com>
2025-04-26 21:17:18 +0500

    render test

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -11,14 +11,21 @@ import (
 
 func main() {
 
-    name := os.Getenv("DBnamedb")
+    name := os.Getenv("DBNamedb")
+    host := os.Getenv("HOSTdb")
+    password := os.Getenv("PASSWORDdb")
+    portdb := os.Getenv("PORTdb")
     
     db :=os.Getenv("USERdb")
 
     log.Println("DBnamedb:", name)
     log.Println("USERdb:", db)
+    log.Println("HOSTdb:", host)
+    log.Println("PASSWORDdb:", password)
+    log.Println("PORTdb:", portdb)
     // Define config file path
-    configFile := flag.String("config", "configs/.env", "Path to configuration file")
+    configFile := flag.String("config", "/etc/secrets/.env", "Path to configuration file")
+    
     flag.Parse()
 
     // Try to load .env file, but don't fail if it doesn't exist (for Render deployment)
