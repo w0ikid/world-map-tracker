49133b81d4057df45aa757351eefc9433e7bbdd3
Doni <laksling@gmail.com>
2025-04-28 02:53:09 +0500

    render + vercel test

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:1488/api'
+    // apiUrl: 'http://localhost:1488/api'
+    apiUrl: 'https://world-map-tracker.onrender.com/api'
   };
